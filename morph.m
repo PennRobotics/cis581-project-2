7128fd50901185c6dcbc27eab4b6dc22c2a7d446
Brian Wright <brianwright@mail.usf.edu>
2016-10-18 14:37:07 -0400

    Almost there

--- morph.m
+++ morph.m
@@ -7,83 +7,102 @@ function [morphed_im] = morph(im1, im2, im1_pts, im2_pts, warp_frac, dissolve_fr
 % dissolve_frac: 1 x M vector containing color blending parameter [0, 1]
 % morphed_im: M cells, each containing a morphed image frame
 
-morphed_im = cell(1, 60);
-figure(2)
+% Check input conditions
+assert(size(warp_frac, 1) == size(dissolve_frac, 1), 'Warp and dissolve vectors have different sizes')
+assert(size(warp_frac, 2) == size(dissolve_frac, 2), 'Warp and dissolve vectors have different sizes')
+assert(size(im1_pts, 1) == size(im2_pts, 1), 'Number of correspondences is not the same for each image')
+assert(size(im1_pts, 2) == size(im2_pts, 2), 'Number of correspondences is not the same for each image')
+assert(size(im1, 3) == 3, 'Color channels missing from source image')
+assert(size(im2, 3) == 3, 'Color channels missing from target image')
 
-for M = 1 : size(warp_frac, 2)
-  [im1_width, im1_height, ~] = size(im1);
-  [im2_width, im2_height, ~] = size(im2);
+morphed_im = cell(1, 60);  % Preallocate and set data type of output variable
 
-  % Control points
-  im_pts_avg = (im1_pts + im2_pts) / 2;
-  tri = delaunay(im_pts_avg(:, 1), im_pts_avg(:, 2));
+[im1_width, im1_height, ~] = size(im1);
+[im2_width, im2_height, ~] = size(im2);
 
-  im_warp_pts = (1 - warp_frac(M)) * im1_pts + (warp_frac(M)) * im2_pts;
+im1_dbl = double(im1);
+im2_dbl = double(im2);
 
+% Match output image size to input image size by padding
+if (im1_width > im2_width)
+  im2 = padarray(im2, [im2_width - im1_width, 0], 'symmetric', 'post')
+elseif (im2_width > im1_width)
+  im1 = padarray(im1, [im1_width - im2_width, 0], 'symmetric', 'post')
+end
+if (im1_height > im2_height)
+  im2 = padarray(im2, [0, im2_height - im1_height], 'symmetric', 'post')
+elseif (im2_height > im1_height)
+  im1 = padarray(im1, [0, im1_height - im2_height], 'symmetric', 'post')
+end
+
+% Delaunay triangulation control points are calculated mid-warp
+im_pts_avg = (im1_pts + im2_pts) / 2;
+tri = delaunay(im_pts_avg(:, 1), im_pts_avg(:, 2));
+num_tri = size(tri, 1);
+
+% Cycle through each desired output frame
+for M = 10 : 10 : size(warp_frac, 2)
+
+  % Preallocate each array
+  current_frame = zeros(im1_width, im1_height, 3, 'double');
   im1_warp = zeros(im1_width, im1_height, 3, 'double');
   im2_warp = zeros(im2_width, im2_height, 3, 'double');
+  T1 = zeros(3, 3, num_tri);
+  T2 = zeros(3, 3, num_tri);
 
-  T1 = zeros(3, 3, size(tri, 1));
-  T2 = zeros(3, 3, size(tri, 1));
+  % Get warp points for the current frame by linear interpolation
+  im_warp_pts = (1 - warp_frac(M)) * im1_pts + (warp_frac(M)) * im2_pts;
 
-%  morphed_im = zeros(im1_width, im1_height, 3, 'double');
+% TODO(brwr):
+%  trimesh(tri,im_warp_pts(:,1),im_warp_pts(:,2))
+%  pause(0.05)
 
-  trimesh(tri,im_warp_pts(:,1),im_warp_pts(:,2))
-  pause(0.05)
+  % Calculate transform matrices for each Delaunay triangle
+  for this_tri_idx = 1 : num_tri
+    this_tri = tri(this_tri_idx, :);
 
-  for n = 1 : size(tri, 1)
-    x = im_warp_pts(tri(n, :), 1);
-    y = im_warp_pts(tri(n, :), 2);
-%    morphed_im(round(x), round(y), [1 1 1]') = 1;
+    a_tri = im1_pts(this_tri, :);
+    b_tri = im_warp_pts(this_tri, :);
+    c_tri = im2_pts(this_tri, :);
+
+    T_tri_start = [a_tri'; 1 1 1];
+    T_tri_warp  = [b_tri'; 1 1 1];
+    T_tri_end   = [c_tri'; 1 1 1];
+
+    T1(:, :, this_tri_idx) = T_tri_start * pinv(T_tri_warp);
+    T2(:, :, this_tri_idx) = T_tri_end   * pinv(T_tri_warp);
   end
 
-
-  for tri_idx = 1 : size(tri, 1)
-    a = im1_pts(tri(tri_idx, :), :);
-    b = im_warp_pts(tri(tri_idx, :), :);
-    c = im2_pts(tri(tri_idx, :), :);
-
-    tri_start = [a'; 1 1 1];
-    tri_warp = [b'; 1 1 1];
-    tri_end = [c'; 1 1 1];
-
-    T1(:, :, tri_idx) = tri_start * inv(tri_warp);
-    T2(:, :, tri_idx) = tri_end * inv(tri_warp);
-  end
-
-%  all_points = [kron(ones(im1_width,1),[1:im1_height]'), repmat([1:im1_height]',im1_width,1)];
-%  tri_idx = tsearchn(im_warp_pts, tri, all_points);
-%  for i = 1 : size(all_points, 1)
-%    tri_idx(i)
+  % TODO(brwr): Instead of nested for-loop)
+%  all_points = [kron([1:im1_width]', ones(im1_height, 1)), repmat([1:im1_height]', im1_width, 1)];
+%  num_points = size(all_points, 1);
+%  for chan = 1 : 3
+%  for pixel_idx = 1 : num_points
+%    im1_loc = T1(:, :, pixel_tri_idx(pixel_idx, :)) * [all_points(pixel_idx, :), 1]';
+%    im2_loc = T2(:, :, pixel_tri_idx(pixel_idx, :)) * [all_points(pixel_idx, :), 1]';
+%    im1_level = interp2(im1_hsv(:, :, chan), im1_loc(1, 1), im1_loc(2, 1));
+%    im2_level = interp2(im2_hsv(:, :, chan), im2_loc(1, 1), im2_loc(2, 1));
+%    im1_warp(all_points(pixel_idx, 2), all_points(pixel_idx, 1), chan) = im1_level;
+%    im2_warp(all_points(pixel_idx, 2), all_points(pixel_idx, 1), chan) = im2_level;
+%  end
 %  end
 
+  parfor i = 130: 200
+    for j = 130: 200
+      pixel = [i, j];
+      pixel_tri_idx = tsearchn(im_warp_pts, tri, pixel);
+      for chan = 1 : 3
+        im1_loc = T1(:, :, pixel_tri_idx) * [pixel, 1]';
+        im2_loc = T2(:, :, pixel_tri_idx) * [pixel, 1]';
+        im1_level = interp2(im1_dbl(:, :, chan), im1_loc(1, 1), im1_loc(2, 1), 'linear');
+        im2_level = interp2(im2_dbl(:, :, chan), im2_loc(1, 1), im2_loc(2, 1), 'linear');
+        im1_warp(j, i, chan) = im1_level;
+        im2_warp(j, i, chan) = im2_level;
+      end
+    end
+  end
 
-%  im1_hsv = rgb2hsv(im1);  % output is type double
-%  im2_hsv = rgb2hsv(im2);
-
-  % x = 1:60;
-  % p = 1.10 ./ (1 + exp(-(-3+0.1*x))) - .05;
-  % q = 1 - p;
-
-  %% IMAGE ONE
-  % Create triangles for the current frame using warp_frac
-
-  % Determine which triangle the current pixel is in using tsearchn
-
-  % Use interp2 to get the correct fraction of colors from neighbors
-% TODO(brwr):  im1_warp = interp2()
-
-  %% IMAGE TWO
-  im2_warp = zeros(im1_width, im1_height, 3, 'double');
-
-
-  % TODO(brwr): Convert to RGB
-
-%  morphed_im = uint8((1 - dissolve_frac) * im1_warp + (dissolve_frac) * im2_warp);
-  current_frame(:, :, 1) = [1 1; 1 1; 0 0; 0 0];
-  current_frame(:, :, 2) = [1 1; 0 0; 1 1; 0 0];
-  current_frame(:, :, 3) = [1 0; 1 0; 1 0; 1 0];
-  morphed_im{M} = current_frame;
-  disp('it')
+  current_frame = (1 - dissolve_frac(M)) * im1_warp + (dissolve_frac(M)) * im2_warp;
+  morphed_im{M} = uint8(current_frame);
 end
 end
